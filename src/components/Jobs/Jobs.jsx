71d2dd85be089ad6dd776919e366506e7d9a18a4
3bdalah <3bdallah.dev@gmail.com>
2023-11-08 04:20:01 +0200

    fix(login):fixed issuee login - added component JobCard - built component jobs

--- src/components/Jobs/Jobs.jsx
+++ src/components/Jobs/Jobs.jsx
@@ -1,7 +1,19 @@
+import { useContext } from "react";
+import { JobsContext } from "../../Context/JobsContext";
+import JobCard from "./../JobCard/JobCard";
+
 export default function Jobs() {
+  const { jobs } = useContext(JobsContext);
+
+  console.log("jobs from context and component jobs", jobs);
   return (
-    <div className="font-mono text-gray-700 text-2xl capitalize">
-      Jobs
+    <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12 mt-10">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-wrap -mx-4">
+          {Array.isArray(jobs) &&
+            jobs.map((job) => <JobCard key={job._id} job={job} />)}
+        </div>
+      </div>
     </div>
   );
 }
